fe259068d69520782ff7f1ddcac3f3f88ecec65b
mauriceoegerli <maurice@oeger.li>
2019-05-01 00:44:06 +0200

    improved player page

--- watch/video.js
+++ watch/video.js
@@ -11,46 +11,64 @@ function loadInfo(data) {
 
     let html = Mustache.to_html(template, data);
     let viewCountString = `${numberWithSeparators(data.viewCount)} Views`;
+    let likeString = numberWithSeparators(data.likeCount);
+    let dislikeString = numberWithSeparators(data.dislikeCount);
 
     let video = $('.video-infobox').html(html);
     $(video).find('.infobox-views').text(viewCountString);
+    $(video).find('.like-count').text(likeString);
+    $(video).find('.dislike-count').text(dislikeString);
+    
     $(video).find('.video-infobox-description').html(data.descriptionHtml);
     $('#channel-img').attr('src', data.authorThumbnails[4].url);
     $(video).removeClass('loading');
+
+    $(video).find('.video-infobox-description').find('a').each((index, element) => {
+        let urlParams = new URLSearchParams($(element).attr('href').split('?')[1]);
+        if (urlParams.has('q')) {
+            let url = urlParams.get('q')
+            $(element).attr('href', url);
+            let originalHTML = $(element).html();
+            $(element).html(`<img class="favicon-link" src="https://www.google.com/s2/favicons?domain=${url}" ></img>${originalHTML}`);
+        }
+    });
 }
 
 function loadVideo() {
     let url = window.location.search;
-    let videoId = url.replace('?v=', '');
+    let urlParams = new URLSearchParams(url);
+    if (urlParams.has('v')) {
+        let videoId = urlParams.getAll('v');
 
-    $.ajax({
-        type: "GET",
-        url: `${baseUrl}videos/${videoId}`,
-        data: {
-            region: defaultRegion
-        },
-        dataType: "JSON",
-        success: function (response) {
-            document.title = `${response.title} - ViewTube`;
-            $('head').append(`<meta property="og:title" content="${response.title} - ViewTube">`);
-            loadInfo(response);
-            let currentVideo = response.formatStreams[0].url;
-            if (response.adaptiveFormats != undefined) {
-                let audioUrls = resolveAudioFormats(response.adaptiveFormats);
-                let currentAudio = audioUrls[audioUrls.length - 1].url;
-                $('#audio').attr('src', currentAudio);
+        $.ajax({
+            type: "GET",
+            url: `${baseUrl}videos/${videoId}`,
+            data: {
+                region: defaultRegion
+            },
+            dataType: "JSON",
+            success: function (response) {
+                document.title = `${response.title} - ViewTube`;
+                $('head').append(`<meta property="og:title" content="${response.title} - ViewTube">`);
+                loadInfo(response);
+                let currentVideo = response.formatStreams[0].url;
+                if (response.adaptiveFormats != undefined) {
+                    let audioUrls = resolveAudioFormats(response.adaptiveFormats);
+                    let currentAudio = audioUrls[audioUrls.length - 1].url;
+                    $('#audio').attr('src', currentAudio);
 
-                let videoUrls = resolveVideoFormats(response.adaptiveFormats);
-                currentVideo = videoUrls[0].url;
+                    let videoUrls = resolveVideoFormats(response.adaptiveFormats);
+                    currentVideo = videoUrls[0].url;
+                }
+                $('.video-mp4').attr('src', currentVideo);
+                let video = document.getElementById('video');
+                let audio = document.getElementById('audio');
+                video.load();
+                audio.load();
+                syncAudioVideo();
             }
-            $('.video-mp4').attr('src', currentVideo);
-            let video = document.getElementById('video');
-            let audio = document.getElementById('audio');
-            video.load();
-            audio.load();
-            syncAudioVideo();
-        }
-    });
+        });
+    }
 }
 
 function syncAudioVideo() {
